2b783e9ee0f901286feb06859b26d78ef23be055
cjsolis <cjelizondo12@gmail.com>
2023-12-28 20:32:44 -0600

    creating sidebar with links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import "./navbar.scss";
+import {motion} from "framer-motion"
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { SiCredly } from "react-icons/si";
+import Sidebar from "../sidebar/Sidebar";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* Sidebar */}
+      <Sidebar/>
       <div className="wrapper">
-        <span>Claudia Solis</span>
+        <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Claudia Solis</motion.span>
         <div className="social">
           <a href="https://github.com/cjsolis12"><FaGithub/></a>
           <a href=""><FaLinkedin/></a>
