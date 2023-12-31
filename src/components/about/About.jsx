cda323e428ef7eb58432b865c9d591bd4729ca8f
cjsolis <cjelizondo12@gmail.com>
2023-12-31 12:21:41 -0600

    edit about me transition with p tag

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,30 +1,65 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import "./about.scss";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 
 const About = () => {
-  const ref = useRef();
+  const controls = useAnimation();
 
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    const windowHeight = window.innerHeight;
 
-  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+    // Adjust these values according to your preference
+    const startAnimationAt = windowHeight / 2;
+    const endAnimationAt = windowHeight * 1.5;
+
+    if (scrollY > startAnimationAt && scrollY < endAnimationAt) {
+      controls.start({ y: 0, opacity: 1 });
+    } else {
+      controls.start({ y: "-100%", opacity: 0 });
+    }
+  };
+
+  useEffect(() => {
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener when the component unmounts
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []); // Empty dependency array to ensure the effect runs only once
 
   return (
-    <div
+    <motion.div
       className="about"
-      ref={ref}
       style={{
-        background:"linear-gradient(180deg, #111132, #2b2669)"}}
+        background: "linear-gradient(180deg, #111132, #2b2669)",
+        overflow: "hidden",
+      }}
     >
-      <motion.h1 style={{ y: yText }}>
+      <motion.h1
+        style={{ y: "-100%", opacity: 0 }}
+        animate={controls}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
         ABOUT ME
       </motion.h1>
+      <motion.p
+        style={{ y: "-60%", opacity: 0 }}
+        animate={controls}
+        transition={{ delay: 0.2, duration: 0.5, ease: "easeInOut" }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </motion.p>
       <motion.div className="watercolor"></motion.div>
-    </div>
+    </motion.div>
   );
 };
 
 export default About;
+
