e3a95e5c0487fc2d01469d8e3d998e217782ecee
cjsolis <cjelizondo12@gmail.com>
2024-01-10 15:28:08 -0600

    added vertical timeline npm package for experience

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import About from "./components/about/About";
 import Portfolio from "./components/portfolio/Portfolio";
+import Resume from "./components/resume/Resume";
 
 const App = () => {
   return (
@@ -17,8 +18,11 @@ const App = () => {
       <section id="Portfolio">
         <Portfolio />
       </section>
+      <section id="Resume">
+        <Resume/>
+        </section>
       <section id="Contact">Contact</section>
-      <section id="Resume">Resume</section>
+     
     </div>
   );
 };
